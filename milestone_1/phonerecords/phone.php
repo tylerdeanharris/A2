f650bb92d9b5ead174f6c08bd41a5008ccc01015
DannyTomasic <danieltomasic@Dannys-MBP.jcu.edu.au>
2015-04-24 11:22:10 +1000

    Revert "updates"
    This reverts commit c64bfbe43a213c04d328848c831ee9a03c1fb7fd.

---
+++ milestone_1/phonerecords/phone.php
@@ -0,0 +1,85 @@
+<?php
+include("dbconnect.php")
+/* Fairly simple example - there's a form for inserting a new phone record and a set of forms, one for each record,
+	that allows for deleting and updating each record. In these ones, the id of the record is passed using a hidden form field. 
+*/
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PHP SQLite Database Example (Phone Records)</title>
+<style type="text/css">
+.subtleSet {
+	border-radius:25px;
+	width: 30em;
+}
+.deleteButton {
+	color: red;
+}
+</style>
+</head>
+
+<body>
+<h1>Phone Database</h1>
+<form id="insert" name="insert" method="post" action="dbprocessphone.php">
+<fieldset class="subtleSet">
+    <h2>Insert new phone record:</h2>
+    <p>
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name">
+    </p>
+    <p>
+      <label for="phone">Phone: </label>
+      <input type="text" name="phone" id="phone">
+    </p>
+        <p>
+      <label for="address">Addresss: </label>
+      <input type="text" name="address" id="address">
+    </p>
+    <p>
+      <input type="submit" name="submit" id="submit" value="Insert Entry">
+    </p>
+</fieldset>
+</form>
+
+<fieldset class="subtleSet">
+<h2>Current data:</h2>
+<?php
+// Display what's in the database at the moment.
+$sql = "SELECT * FROM people";
+foreach ($dbh->query($sql) as $row)
+{
+?>
+<form id="deleteForm" name="deleteForm" method="post" action="dbprocessphone.php">
+<label>id</label>
+<?php
+	echo "<input type='text' name='id' value='$row[id]' size='1' />";
+?>
+<label>Name:</label> 
+<?php
+	echo "<input type='text' name='name' value='$row[name]' />";
+?>
+<label>Phone:</label>
+<?php
+	echo "<input type='text' name='phone' value='$row[phone]' />";
+?>
+<label>Address:</label>
+<?php
+	echo "<input type='text' name='address' value='$row[address]' />";
+?>
+<?php
+	echo "<input type='hidden' name='id' value='$row[id]' />";
+?>
+<input type="submit" name="submit" value="Update Entry" />
+<input type="submit" name="submit" value="Delete Entry" class="deleteButton">
+<input type="submit" name="submit" value="X" class="deleteButton">
+</form>
+<?php
+}
+echo "</fieldset>\n";
+// close the database connection
+$dbh = null;
+?>
+</body>
+</html>
