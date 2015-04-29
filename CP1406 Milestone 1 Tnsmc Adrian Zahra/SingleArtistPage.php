341d0d3ad3584816f328c5c1f2586f2a1adf6658
secretsecret <adrian.zahra@my.jcu.edu.au>
2015-04-30 09:16:26 +1000

    Draft html artist page
    This is the unfinished Draft Version of the html comp artist page it
    contains all running files

---
+++ CP1406 Milestone 1 Tnsmc Adrian Zahra/SingleArtistPage.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Townsvill Music Center</title>
+<link href="SingleArtistPage.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<header id="Pageheader">
+<h1>Townsvill Comunity Music Centre</h1>
+</header>
+</body>
+</html>
