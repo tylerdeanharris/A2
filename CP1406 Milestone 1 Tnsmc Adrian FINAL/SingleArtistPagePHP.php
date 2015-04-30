b8b270ac472a79d7e4f801d9ddacab176a964781
adrianZahra <adrian.zahra@my.jcu.edu.au>
2015-04-30 15:40:05 +1000

    Final copy of the mile stone 1

---
+++ CP1406 Milestone 1 Tnsmc Adrian FINAL/SingleArtistPagePHP.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Townsvill Music Center</title>
+<link href="SingleArtistPage.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<header id="Pageheader">
+<h1 id="title">Townsvill Comunity Music Centre</h1>
+<h1 id="pagename">Atist: Aviva Quartet </h1>
+ <img src="TCMC98Neg.gif" class="logo"/>
+ <nav class="navbar">
+ 	<ul>
+		<li><span title="Home Page"><a href="https://www.google.com.au/?gws_rd=ssl">Home</a></li>
+		<li><span title="Events Page"><a href="https://www.google.com.au/?gws_rd=ssl">Events</a></li>
+		<li><span title="Bulletin Board Page"><a href="https://www.google.com.au/?gws_rd=ssl">Board</a></li>
+		<li><span title="Artist Listing Page"><a href="https://www.google.com.au/?gws_rd=ssl">Artist</a></li>
+        <li><span title="About Us Page"><a href="https://www.google.com.au/?gws_rd=ssl">About us</a></li>
+        <li><span title="Sign In/ SIgn Up Page"><a href="https://www.google.com.au/?gws_rd=ssl">Signin/Sign up</a></li>
+	</ul>
+ </nav>
+</header>
+<section class="pagebody">
+<article class="artist">
+<h1 id="artisttitle">
+Aviva Quartet
+</h1>
+<p id="centered1"><strong>Four talented musicians combine their skills in a variety of musical styles including fiery Rhumbas, Tangos, Serenades - a mixture of the most devine and swoon-worthy pieces, guaranteed to delight the senses.</strong>
+<img src="AVIVA-LANE500.jpg" class="bandimg" span title="The Aviva Quartet Band"/></p>
+<p>
+The group features Caroline Lloyd-Doolan and Susan Fraser on Violin, Ivy Wu on Cello and Jessica Winton on viola. 
+
+Caroline is a well known performer both locally and regionally and Susan is also well known on the local music scene. 
+Ivy hails from Brisbane having completed her studies in France. 
+Jessica has come back to Townsville after completing her degree in Tasmania. </p>
+<p>
+The girls all have a passion for chamber music and are very excited to be able to form up a group that will be able to perform regularly both locally and in the surrounding regions. 
+Their concert will take listeners through a wonderful journey of beautiful melodies and the most romantic music that is guaranteed to leave the senses fulfilled and wanting more. 
+</p>
+</article>
+<article class="sidebar">
+<h1 id="contdet">
+Want to see more?
+</h1>
+<p> If you wish to further wiew the the artists performing at the <em>Townsvill Comunity Music Center</em> you can ckick <a href="https://www.google.com.au/?gws_rd=ssl"<strong>Here!</strong></a>to go back to our list of artist.
+</p>
+<p>
+To find out when <em>Aviva Quartet</em> will perform at the center you can click <a href=" https://www.google.com.au/?gws_rd=ssll"<strong>Here!</strong></a> To go to your events calender to find out.
+</p>
+<p>
+If you have any further enquireies<br>
+call us on, Phone:07 4724 2086
+Mobile:0402 255 182 or send us an email at: admin@townsvillemusic.org.au 
+ </p>
+ 
+ 
+</article>
+</section>
+<footer class="footerouter">
+<section class="footer">
+<img src="QG Small.gif" id="TNSlogo1"/>
+<img src="TCCcolour150193 Small.gif" id="TNSlogo2"/>
+<p>Contact Details:
+<br>
+Phone   07 4724 2086
+Mobile    0402 255 182
+<br>
+Postal Address: PO Box 1006, Townsville, Qld 4810
+<br>
+Address: Townsville Civic Theatre, 41 Boundary Street, Townsville, Qld 4810
+</p>
+</section>
+</section>
+</body>
+</html>
