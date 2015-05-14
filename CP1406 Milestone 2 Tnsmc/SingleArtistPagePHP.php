2daa597d8f6566ecc638795561bcea5295d9ee62
adrianZahra <adrian.zahra@my.jcu.edu.au>
2015-05-15 07:37:31 +1000

    Revised files

---
+++ CP1406 Milestone 2 Tnsmc/SingleArtistPagePHP.php
@@ -0,0 +1,155 @@
+@charset "utf-8";
+html{
+	Background-color:#06F;	
+}
+#Pageheader{
+	background-color:#000;
+	color:#FFF;
+	font-family:Palace Script MT;
+	text-align:left;
+	padding:0.5%;
+	font-size:40px;
+	border-radius: 8px;
+}
+
+#title{
+	margin-left: 13%;
+	margin-right: 13%;
+}
+
+#pagename{
+	margin-left: 33%;
+	margin-right: 33%;
+	font-size: 110%;
+	margin-top: -2%;
+}
+
+.logo{
+	float:left;
+	margin-right: 73%;
+	margin-left: 73%;
+	margin-top:-10%;
+}
+
+.navbar{
+	width: 60%;
+    height: 100%;
+    font-size: 16px;
+    font-family: Arial;
+    text-align: center;
+    background-color:#666;
+    border-radius: 8px;
+	margin-left: 20%;
+	margin-right: 20%;
+}
+
+.navbar ul {
+    height: auto;
+	width:auto;
+    padding: 30px;
+    margin: 0px;
+}
+
+.navbar li { 
+	display: inline; 
+	padding: 15px; 
+	border:#000;
+	border-style:solid;
+	background-color:#000;
+	border-radius: 5px;
+}
+
+.navbar a {
+	color:#FFF;
+	text-decoration:none;
+	font-weight: bold;
+}
+
+.pagebody{
+	padding-top:1%;
+}
+
+
+.artist {
+	border-color:#000;
+	border-radius: 8px;
+	background-color:#FFF;
+	border-style:solid;
+	width:60%;
+	height:100%;	
+	border-width:4px;
+	float:left;
+	text-align:left;
+	padding:1%;
+}
+
+#artisttitle{
+	font-family:calibri;
+	font-style:italic;
+	font-size:300%;
+	text-align:center;
+	margin-top:.1%;
+}
+
+#centered1 {
+	text-align:center;
+}
+
+.sidebar{
+	border-color:#000;
+	border-radius: 8px;
+	background-color:#FFF;
+	border-style:solid;
+	width:34%;
+	height:100%;	
+	border-width:4px;
+	float:right;
+	padding-left:1%;
+	padding-right:1%;
+	margin-top:.1%;
+}
+
+.bandimg {
+	width:50%;	
+	float:left;
+	padding-top:1%;
+	padding-right:1%;
+	border-color:#0C0;
+}
+
+.footer{
+	margin-top: 1%;
+	border-color:#000;
+	border-radius: 8px;
+	background-color:#FFF;
+	border-style:solid;
+	width:100%;		
+	border-width:4px;
+	float:left;
+	text-align:left;
+	padding-top:1%;
+	padding-bottom:1%;
+	
+}
+
+.footerouter{
+	margin-bottom:40%;
+	width:99.5%;
+}
+
+#TNSlogo1{
+	Float:left;
+	padding:1%;
+}
+
+#TNSlogo2{
+	float:left;
+	padding:1%;
+}
+
+#contdet{
+	font-family:calibri;
+	font-style:italic;
+	font-size:30px;
+	text-align:center;
+}
